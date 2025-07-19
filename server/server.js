e61a905e3580c4b55f76b913f5647d962fe60958
MansiGohil-dev <gohilmansi523@gmail.com>
2025-07-19 10:36:27 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -114,3 +114,10 @@ app.delete('/api/products/:id', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send({
+    activeStatus: true,
+    error: false
+  });
+});
